06af9c30517bac23ba01c325a96af154df4ee745
shruti8302 <8302+shruti8302@users.noreply.github.com>
2025-08-03 16:29:09 +0530

    first react app

---
+++ first-react-code/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return <p>You have read {props.count} pieces of advice</p>;
+}
